3e146e60c240db26bdccdbc39c8aa7871437d093
moneer2020 <72842828+moneer2020@users.noreply.github.com>
2021-03-08 01:46:23 -0800

    My second automation project commit

--- src/main/java/com/collection/Main.java
+++ src/main/java/com/collection/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         for(int i = 0; i <5 ; i++){
+=======
+        for(int i = 0; i <3 ; i++){
+>>>>>>> My second automation project commit
             System.out.println("Hello Batch 8");
             Thread.sleep(2000);
         }
